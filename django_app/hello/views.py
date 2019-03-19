44aadabae45cb313c10ce9da0e06a0d9e13b4fa7
shota <ryu5791@yahoo.co.jp>
2019-03-20 06:59:16 +0900

    test

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello Django!!!")
+	msg = request.GET['msg']
+	return HttpResponse('you typed: "' + msg + '".')
 
