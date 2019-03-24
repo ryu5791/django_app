676c2de38d0de3669d8df4bc653011176c7ddaa8
M.Ichiryu <ryu5791@yahoo.co.jp>
2019-03-25 03:50:03 +0900

    ~p111

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from .forms import HelloForm
 
-def index(request):
-	params = {
-		'title20'		:'Hello',
-		'message20'		:'your data:',
-		'form20'		:HelloForm(),
-	}
-	if (request.method == 'POST'):
-		params['message20'] = '名前：' + request.POST['name1'] + \
-			'<br>メール：' + request.POST['mail1'] + \
-			'<br>年齢：' + request.POST['age1']
-		params['form20'] = HelloForm(request.POST)
-	return render(request, 'hello/index.html', params)
+class HelloView(TemplateView):
+
+	def __init__(self):
+		self.params = {
+			'title20'		:'Hello',
+			'message20'		:'your data:',
+			'form20'		:HelloForm(),
+		}
+
+	def get(self, request):
+		return render(request, 'hello/index.html', self.params)
+
+	def post(self, request):
+		msg = 'あなたは、<b>' + request.POST['name1'] + \
+			'(' + request.POST['age1'] + \
+			')</b>さんです。<br>メールアドレスは<b>' + request.POST['mail1'] + \
+			'</b>ですね。'
+		self.params['message20'] = msg
+		self.params['form20'] = HelloForm(request.POST)
+		return render(request, 'hello/index.html', self.params)
 
