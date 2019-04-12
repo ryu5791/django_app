ffcde48be65a63bd8c72dbfe597ad37ad025909a
shota <ryu5791@yahoo.co.jp>
2019-04-12 17:43:20 +0900

    jsonファイル読み込み

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
 import logging
+import json
 
 def index1(request):
 
-	logging.debug('debug message')
+
+	temp = open('Score2019_1.json',encoding="utf-8_sig")
+	tbl_score = json.load(temp)
+
+	num=0
+	for src in tbl_score["results"]:
+		num += 1
+
+	logging.debug(num)
 
 	data5 = Friend.objects.all()
 	params = {
