871bc8c345ae03a50b82b6c999741d6d188267bc
ryu5791 <ryu5791@yahoo.co.jp>
2019-10-16 06:29:49 +0900

    タブテスト

--- tennis_app/score/views.py
+++ tennis_app/score/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+import logging
+
+@login_required(login_url='/admin/login/')
+def index(request):
+    test = User.objects.filter(username=request.user)
+    logging.debug("index user:" + str(request.user))
+    logging.debug("index is_super:" + str(request.user.is_superuser))
+    logging.debug("index test:" + str(test))
+    logging.debug("index has_perm:" + str(request.user.has_perm("score.add_tblscore") ))
+#   logging.debug("index has_perm:" + str(request.user.has_perm("auth.add_user") ))
+    params = {'login_user': request.user,}
+    return render(request, "score/index.html", params)
+
+
